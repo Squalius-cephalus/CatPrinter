96765748ff41dce0eddd43a09eaaeebec1271109
Squalius-cephalus <juho.taskinen@edu.turkuamk.fi>
2023-05-03 15:24:41 +0300

    Fixes and more comments

--- gui.py
+++ gui.py
@@ -3,20 +3,25 @@ from tkinter import *
 import tkinter as tk
 from PIL import ImageTk, Image, ImageOps
 from tkinter import filedialog
+# TODO: rename print2 to something better.
 import print2
 import tempfile
 import yaml
+
+
 with open("config.yaml", "r") as f:
     config = yaml.load(f, Loader=yaml.FullLoader)
 printer_width = config["printer_width"]
 
 root = Tk()
 root.geometry("800x600")
+root.title("Cat Printer")
 root.resizable(width=False, height=False)
 
 # Create a global variable to store the panel object
 panel = None
 img = None
+# TODO: Make better "blank" image!
 temp_path = "Untitled.png"
 
 default_img = Image.new(
@@ -28,7 +33,9 @@ def save_settings(device_name, contrast, feed_lines, printer_width):
 
     # Yes, this is not good way to do this.
     config["bluetooth_name"] = device_name
-
+    config["contrast"] = contrast-1
+    config["feed_lines"] = int(feed_lines)
+    config["printer_width"] = int(printer_width)
     with open("config.yaml", "w") as new_file:
         yaml.dump(config, new_file)
 
@@ -43,8 +50,11 @@ def open_settingswindow():
 
     device_name = tk.Entry(settings_window)
     device_name.insert(0, config["bluetooth_name"])
-    contrast = tk.Entry(settings_window)
-    contrast.insert(0, config["contrast"])
+
+    contrast = Scale(settings_window, from_=1, to=3, orient=HORIZONTAL)
+    contrast.set(config["contrast"]+1)
+    # contrast = tk.Entry(settings_window)
+    # contrast.insert(0, config["contrast"])
     feed_lines = tk.Entry(settings_window)
     feed_lines.insert(0, config["feed_lines"])
     printer_width = tk.Entry(settings_window)
@@ -83,6 +93,8 @@ def open_img():
     global panel  # Access the global panel object
     global img
     global temp_path
+
+    # TODO: Too small images with alpha channel does weird things. Scale images to bigger than printers width.
 
     x = openfn()
     img = Image.open(x)
@@ -125,6 +137,8 @@ def ConnectingToPrinter(print_data):
             time.sleep(1)  # import time
 
         print_failed = True
+
+    # TODO: Fix this, sometimes this does not fail but still sets "print_failed" true
 
     if print_failed:
         set_status(f"WARNING! Printer not found!")
